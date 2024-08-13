a519ccf38aa0a41a9254ad5360cd4b7a1c23518e
Usama Khan <161131380+Usama209262@users.noreply.github.com>
2024-08-13 09:40:27 +0000

    container config done

---
+++ js-basic/01-arrow-function.js
@@ -0,0 +1,17 @@
+const user = {
+    username:"usama",
+    price : 20000,
+    laptop : "Dell 12th Gen",
+    welcomeMassage : function(){
+        console.log(`${this.price},welcome to my website`);
+        // console.log(this);
+        
+    }
+}
+// user.welcomeMassage ()
+// user.price = 100000
+// user.welcomeMassage ()
+
+console.log(user)       
+
+
--- js-basic/test.js
+++ js-basic/test.js
@@ -1,2 +1,1 @@
-console.log("Usama");
-console.log("Khan");
+
