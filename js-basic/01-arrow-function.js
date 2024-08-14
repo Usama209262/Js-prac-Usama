ebc55d7ec2199dd6748b1866270b4a54d550e485
Usama Khan <161131380+Usama209262@users.noreply.github.com>
2024-08-14 10:13:20 +0000

    container config done

--- js-basic/01-arrow-function.js
+++ js-basic/01-arrow-function.js
@@ -1,17 +1,52 @@
-const user = {
-    username:"usama",
-    price : 20000,
-    laptop : "Dell 12th Gen",
-    welcomeMassage : function(){
-        console.log(`${this.price},welcome to my website`);
-        // console.log(this);
+// This use hota hai current context k liye 
+
+// const user = {
+//     username:"usama",
+//     price : 20000,
+//     laptop : "Dell 12th Gen",
+//     welcomeMassage : function(){
+//         console.log(`${this.username},welcome to my website`);
+//         // console.log(this);
         
-    }
+//     }
+// }
+// user.welcomeMassage ()
+// user.username = "Subhaan"
+// user.welcomeMassage ()
+
+// console.log(this)       
+
+// browser mai jo bhi sab se ziyada global object hai wo window hai 
+
+
+const addOne = function () {
+    let username = "Usama Khan"
+    console.log(this.username);
 }
-// user.welcomeMassage ()
-// user.price = 100000
-// user.welcomeMassage ()
+// addOne()
 
-console.log(user)       
+const one = () => {
+    let username = "Usama Khan"
+    console.log(this);
+}
+// one()
 
+// ARROW FUNCTION NORMAL ARROW FUNCTION 
 
+// const addTwo = (num1 , num2) => {
+// return num1 + num2 
+// }
+// console.log(addTwo(23,5))
+
+// Implicit return 
+
+// const addTwo = (num1 , num2) => num1 + num2
+
+// yeh technic react mai use hoga 
+// const addTwo = (num1 , num2) => (num1 + num2)
+
+// Object ko return karne k liye apko parenthesis
+
+const addTwo = (num1 , num2) => ({username : "UsamaKhan"})
+    
+    console.log(addTwo(23,5))
