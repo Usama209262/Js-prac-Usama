4b53fe586735c78a7e94f6b833ab148e22434754
Usama Khan <161131380+Usama209262@users.noreply.github.com>
2024-08-24 07:05:42 +0000

    container config done

---
+++ control_flow/01.js
@@ -0,0 +1,26 @@
+// ====  IF 
+
+// const Chai = true
+// const tempreture = 41
+// if ( tempreture < 40 ) {
+//     console.log("Tempreture Less Then 50");
+// }
+// else {
+//     console.log("tempreture is greter than 50");
+// }
+// console.log("Execute");
+
+// <, >, <= , >= , == , != , === , !== ,
+
+// ( Block Scope) That is the Scope {} mai jo content hai varible hai uska scope bass () mail hi hai except var 
+
+// const score = 200 
+
+// if (score > 100) {
+//     let power = "Fly"
+//     console.log(`User Power is ${power}`);
+// }
+
+const balance = 1000; 
+
+if (balance > 500) console.log("Test");
